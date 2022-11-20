b27370373cb83741c9c0cb9c34e20bc022b1871f
Abdul <42898187+afgunz@users.noreply.github.com>
2022-11-20 15:05:46 -0500

    Create countSumMean.js

---
+++ countSumMean.js
@@ -0,0 +1,73 @@
+/* Question 0
+ *
+ *  Implement the functions defined below
+ *
+ */
+
+/* ===========================================================================
+ * COUNT - the number of items in a list
+ *
+ * For example:
+ *
+ *    count([6,2,3,4,9,6,1,0,5]);
+ *
+ * Returns:
+ *
+ *    9
+ */
+const count = function(arr) {
+  console.log(arr.length);
+  return arr.length;
+};
+
+/* ===========================================================================
+ * SUM - the sum of the numbers in a list
+       - safe to assume that all items are numbers already
+ *
+ * For example:
+ *
+ *    sum([6,2,3,4,9,6,1,0,5])
+ *
+ * Returns:
+ *
+ *    36
+ */
+const sum = function(arr) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+  console.log(result);
+  return result;
+};
+
+// To be used by mean. Do not alter.
+const round = function(number) {
+  return Math.round(number * 100) / 100;
+};
+
+/* ===========================================================================
+ * MEAN - the average value of numbers in a list
+ *      - use the provided 'round' function when returning your value
+ *      - if empty array, return null to indicate that mean cannot be calculated
+ *
+ * For example:
+ *
+ *    mean([6,2,3,4,9,6,1,0,5])
+ *
+ * Returns:
+ *
+ *    4
+ */
+const mean = function(arr) {
+  if (arr.length === 0) {
+    return null;
+  }
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.length !== 0) {
+      sum += arr[i];
+    }
+  }
+  return sum / arr.length;
+};
